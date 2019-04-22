917219ab4a93e3c0aeca351f3388fd2ab6326863
Tram Nguyen <nguyenthiquetram272@gmail.com>
2019-04-23 03:47:05 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-second-app';
+  loadedFeature='recipe';
+  
+ onNavigate(feature:string){
+   this.loadedFeature= feature;
+
+ }
 }
+ 
